beee3cf6912cf4bb3919af5358166ac17613d29d
Phí Hồng Sơn <hongson15102002@gmail.com>
2023-08-16 21:05:44 +0700

    phs-16/8

--- apps/models.py
+++ apps/models.py
@@ -15,7 +15,6 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
-
 class Product(models.Model):
     category = models.ManyToManyField(Category, related_name='product')
     name = models.CharField(max_length=255, null=True)

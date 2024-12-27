ade943a9ef11038c1cfccc94059365f038d3c597
phs <hongson15102002@gmail.com>
2024-12-27 14:37:20 +0700

    abc

--- apps/admin.py
+++ apps/admin.py
@@ -1,13 +1,41 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import *
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Order)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'get_price', 'get_image')
+
+    @admin.display(description='Price')
+    def get_price(self, obj):
+        return obj.price
+
+    @admin.display(description='Image')
+    def get_image(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="width: 50px; height: 50px;" />', obj.image.url)
+        return "No Image"
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'complete', 'date_order')
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'product', 'order', 'quantity', 'date_added')
-    list_per_page = 200
+    list_display = ('id', 'product', 'order', 'get_price', 'quantity', 'date_added', 'get_total')
+
+    @admin.display(description='Total Price')
+    def get_total(self, obj):
+        return obj.get_total
+
+    @admin.display(description='Price')
+    def get_price(self, obj):
+        return obj.product.price if obj.product else 'N/A'
+
+
 
 # admin.site.register(InformationCustomer)
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
